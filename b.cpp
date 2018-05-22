a4445fa9633be718a6e8419d86a017568ad2e0d6
Tatsurou Miyazaki <tatsumiya05@gmail.com>
2018-05-22 09:58:53 +0900

    upload a file

--- b.cpp
+++ b.cpp
@@ -12,9 +12,29 @@
 
 using namespace std;
 #define vsort(v) sort(v.begin(), v.end());
-#define vunique(v) unique(v.begin(), v.end());
 
 int main(){
 	cin.tie(0);
 	ios::sync_with_stdio(false);
+
+	int X;
+	cin >> X;
+	bool array[X+1];
+	for(int i = 0;i <= X; i++) array[i] = false;
+
+	for(int i = 2; i <= X; i++) {
+		int tmp = i * i;
+		while(tmp <= X) {
+			array[tmp] = true;
+			tmp *= i;
+		}
+	}
+	for(int i = X; i >= 1; i--) {
+		if(array[i]) {
+			cout << i << endl;
+			return 0;
+		}
+	}
+	cout << 1 << endl;
+	return 0;
 }
