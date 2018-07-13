f8f48f1d91852fa58ee35d47b156d2e59a6595b5
Tatsurou Miyazaki <tatsumiya05@gmail.com>
2018-07-13 20:47:37 +0900

    update a file

--- template.cpp
+++ template.cpp
@@ -12,9 +12,13 @@
 #include <cstring>
 
 using namespace std;
+typedef pair<int, int> P;
+#define mp make_pair
 #define vsort(v) sort(v.begin(), v.end());
 #define vunique(v) unique(v.begin(), v.end());
-#define INF 2000000000
+#define INF 1e18
+#define MAX_V 10000
+
 
 int main(){
 	cin.tie(0);
