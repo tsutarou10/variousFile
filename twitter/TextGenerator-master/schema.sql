29be34a587791105dddde7e8d63337994d33e47d
tatsuro miyazaki <tatsumiya05@gmail.com>
2019-02-06 00:37:20 +0900

    update

--- twitter/TextGenerator-master/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists chain_freqs;
-create table chain_freqs (
-    id integer primary key autoincrement not null,
-    prefix1 text not null,
-    prefix2 text not null,
-    suffix text not null,
-    freq integer not null
-);
--- twitter/twitter_data/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 11321,
-  "created_at" : "2018-11-01 07:53:45 +0000",
-  "lang" : "ja"
-}
