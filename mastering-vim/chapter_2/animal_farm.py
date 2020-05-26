c01b464af67e80f2a9eeb34d30009195c9664406
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2020-05-26 23:57:01 +0900

    Update mastering vim

--- mastering-vim/chapter_2/animal_farm.py
+++ mastering-vim/chapter_2/animal_farm.py
@@ -5,17 +5,17 @@
 import sys
 
 def add_animal(farm , animal):
-    farm.add(animal)
+  farm.add(animal)
     return farm
 
 def main(animals):
-    farm = set()
+  farm = set()
     for animal in animals:
-        farm = add_animal(farm, animal)
-    print("We've got some animals on the farm:", ', '.join(farm) + '.')
+      farm = add_animal(farm, animal)
+      print("We've got some animals on the farm:", ', '.join(farm) + '.')
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Pass at least one animal type!')
-        sys.exit(1)
+t if len(sys.argv) == 1:
+    print('Pass at least one animal type!')
+    sys.exit(1)
     main(sys.argv[1:])
